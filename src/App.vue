6fe3c3f8a3e8721ffe193a928dc41e8d3bce9d04
Cristiane Bastos <cristiane.bastos@gmail.com>
2020-12-12 13:56:58 -0300

    Inserção de Título e descrição

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Listagem />
+    <Listagem/>
     <BoasVindas />
   </div>
 </template>
--- src/components/BoasVindas.vue
+++ src/components/BoasVindas.vue
@@ -1,16 +1,21 @@
 <template>
-  <div></div>
+    <div class="boas-vindas">
+        <h2>Noel Online</h2>
+
+        <p>Você já pensou em fazer algo impactante nesse natal mesmo a distância? Existem muitas crianças escrevendo para o bom velhinho sobre tudo que desejam e esperaram o ano todo. Convido você a ter a experiência de adotar um sonho e ajudar a realizá-lo para a criação um Natal mais completo e feliz. Seja participante! Selecione entre as cartas abaixo e seja o tão desejado Papai Noel de uma criança.</p>
+    </div>
+
+    
+
 </template>
 
 <script>
 export default {
-  name: "BoasVindas",
-  /*props: {
-    msg: String,
-  },*/
-};
+    name: 'BoasVindas'
+    
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
