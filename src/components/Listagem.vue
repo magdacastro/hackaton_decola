0f678ebee5f2866adad3a9cc23b7443df48c6615
Magda Castro <magda@gmail.com>
2020-12-12 14:30:06 -0300

    Feature da listagem das cartas

--- src/components/Listagem.vue
+++ src/components/Listagem.vue
@@ -1,13 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <button @click="puxarCartas">Trazer Cartas</button>
+    <div v-for="carta in cartas" :key="carta.name">
+      <p>Criança: {{ carta.name }}</p>
+      <p>Endereço: {{ carta.address }}</p>
+      <p>Carta: {{ carta.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Listagem",
-  /*props: {
-    msg: String,
-  },*/
+  data() {
+    return {
+      cartas: {},
+    };
+  },
+  methods: {
+    puxarCartas() {
+      fetch("https://it3yui.firebaseio.com/natal.json")
+        .then((response) => response.json())
+        .then((json) => {
+          this.cartas = json;
+        });
+    },
+  },
 };
 </script>
 
