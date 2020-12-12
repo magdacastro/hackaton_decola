4b92acca4a69ff06cb56c0327f2de38714a9fff5
Magda Castro <magda@gmail.com>
2020-12-12 16:16:00 -0300

    Reajuste de endereço

--- src/components/BoasVindas.vue
+++ src/components/BoasVindas.vue
@@ -1,27 +1,23 @@
 <template>
+  <div class="boas-vindas">
+    <h2>Noel Online</h2>
 
-
-    <div class="boas-vindas">
-        <h2>Noel Online</h2>
-
-        <p>Você já pensou em fazer algo impactante nesse natal mesmo a distância? Existem muitas crianças escrevendo para o bom velhinho sobre tudo que desejam e esperaram o ano todo. Convido você a ter a experiência de adotar um sonho e ajudar a realizá-lo para a criação um Natal mais completo e feliz. Seja participante! Selecione entre as cartas abaixo e seja o tão desejado Papai Noel de uma criança.</p>
-
-
-    </div>
-
-    
-
-    
-
+    <p>
+      Você já pensou em fazer algo impactante nesse natal mesmo a distância?
+      Existem muitas crianças escrevendo para o bom velhinho sobre tudo que
+      desejam e esperaram o ano todo. Convido você a ter a experiência de adotar
+      um sonho e ajudar a realizá-lo para a criação um Natal mais completo e
+      feliz. Seja participante! Selecione entre as cartas abaixo e seja o tão
+      desejado Papai Noel de uma criança.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'BoasVindas'
-    
-}
+  name: "BoasVindas",
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/components/Listagem.vue
+++ src/components/Listagem.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <button @click="puxarCartas">Trazer Cartas</button>
-    <div v-for="carta in cartas" :key="carta.name">
-      <p>Criança: {{ carta.name }}</p>
-      <p>Endereço: {{ carta.address }}</p>
-      <p>Carta: {{ carta.description }}</p>
+  <v-container>
+    <div class="list">
+      <v-btn
+        class="btn-show"
+        color="red"
+        dark
+        large
+        elevation="2"
+        @click="puxarCartas"
+        >Visualizar cartas</v-btn
+      >
+      <!-- <button @click="puxarCartas">Trazer Cartas</button>-->
+      <div v-for="carta in cartas" :key="carta.name">
+        <h3 class="nome">{{ carta.name }}</h3>
+        <p class="address">Endereço: {{ carta.address }}</p>
+        <p class="carta">" {{ carta.description }} "</p>
+        <v-btn class="btn-adote" elevation="2">Adotar esta carta</v-btn>
+      </div>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
@@ -19,15 +31,45 @@ export default {
   },
   methods: {
     puxarCartas() {
-      fetch("https://it3yui.firebaseio.com/natal.json")
+      fetch("https://it3yui.firebaseio.com/natal.json").then((response) => {
+        response.json().then((data) => {
+          let CartasNovoArray = data.map((target) => {
+            if (!target.address.match(/^[A-Zãáéêíîõóú\sa-z]*\s-\s/)) {
+              target.address = target.address
+                .match(/[A-Zãáéêíîõóú\sa-z]*$/)[0]
+                .trim()
+                .replace("o S", "o - S");
+            }
+            return target;
+          });
+          this.cartas = CartasNovoArray;
+        });
+      });
+
+      /*fetch("https://it3yui.firebaseio.com/natal.json")
         .then((response) => response.json())
         .then((json) => {
           this.cartas = json;
-        });
+        });*/
     },
   },
 };
 </script>
 
 <style scoped>
+.list {
+  text-align: center;
+}
+
+.nome {
+  margin-top: 30px;
+}
+
+.carta {
+  font-style: italic;
+}
+
+.btn-adote {
+  margin-bottom: 30px;
+}
 </style>
